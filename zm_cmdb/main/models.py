903dea01ed9d2a14ac8ab8ff99e17e43620f76f5
gqdw <aca_jingru@qq.com>
2016-05-13 14:08:41 +0800

    add Host model

--- zm_cmdb/main/models.py
+++ zm_cmdb/main/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Host(models.Model):
+	hostname = models.CharField(max_length=30, unique=True)
+	eth0 = models.GenericIPAddressField(unique=True)
+	eth1 = models.GenericIPAddressField(blank=True, null=True )
+
