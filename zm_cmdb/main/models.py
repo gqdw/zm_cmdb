b679c0209cc7620e642066e1a60bbd4f33a3d4c8
gqdw <aca_jingru@qq.com>
2016-05-14 14:16:58 +0800

    add HostManager

--- zm_cmdb/main/models.py
+++ zm_cmdb/main/models.py
@@ -3,8 +3,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class HostManager(models.Manager):
+	def create_host(self, hostname, eth0, eth1):
+		host = self.create(hostname=hostname, eth0=eth0, eth1=eth1)
+		return host
+
+
+
 class Host(models.Model):
 	hostname = models.CharField(max_length=30, unique=True)
 	eth0 = models.GenericIPAddressField(unique=True)
 	eth1 = models.GenericIPAddressField(blank=True, null=True )
+	
+	objects = HostManager()
+
+#	@classmethod
+#	def create(cls, hostname, eth0, eth1):
+#		host = cls(hostname=hostname, eth0=eth0, eth1=eth1)
+#		return host
+
 
