051a29b58bea2aeabc8afdc4c338dc8d3d9da8ed
gqdw <aca_jingru@qq.com>
2016-05-14 14:27:37 +0800

    add import view

--- zm_cmdb/main/views.py
+++ zm_cmdb/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Host
+import csv
 
 # Create your views here.
+
+def import_data(request):
+	with open('/Users/aca/Downloads/ecs_list0513.csv', 'rU') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			# print row[0]
+			Host.objects.create_host(row[0], row[2], row[1])
+	return HttpResponse('ok')
