b98a02471bce4abf9d5ece34ebe86f119e3bc927
gqdw <aca_jingru@qq.com>
2016-05-17 18:15:08 +0800

    add search_fields in admin

--- zm_cmdb/main/admin.py
+++ zm_cmdb/main/admin.py
@@ -3,5 +3,8 @@ from .models import Host
 # Register your models here.
 class HostAdmin(admin.ModelAdmin):
 	list_display = ('hostname', 'eth0', 'eth1')
+	search_fields = ('hostname', 'eth0', 'eth1')
+	#list_filter = ('hostname',)
+	# fields = ('hostname', 'eth0', 'eth1')
 
 admin.site.register(Host, HostAdmin)
