6b865301eb81fe2c7362eb45b8c7bcf7e1b830dc
gqdw <aca_jingru@qq.com>
2016-05-20 11:06:46 +0800

    init commit

---
+++ zm_cmdb/main/zabbix.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import ConfigParser
+import os
+
+
+# headers = {'Content-Type': 'application/json-rpc'}
+
+# print auth
+
+class Zabbix():
+	def __init__(self):
+		self.auth = ''
+		self.z_url = 'https://zabbix.zhai.me/api_jsonrpc.php'
+		self.times = 1
+
+	def commit(self, data):
+		res = requests.post(self.z_url, json=data).json()
+		self.times += 1
+		return res
+
+
+	def get_auth(self):
+		config = ConfigParser.ConfigParser()
+		config.read('zabbix.cfg')
+		user = config.get('main', 'user')
+		password = config.get('main', 'password')
+		a_data = {"jsonrpc":"2.0","method":"user.login","params":{"user":user, "password":password } ,"id":1}
+		# self.auth = requests.post(self.z_url, json=a_data).json()['result']
+		self.auth = self.commit(a_data)['result']
+
+	def get_hostip(self):
+		a_data = {
+    "jsonrpc": "2.0",
+    "method": "hostinterface.get",
+    "params": {
+        "output": "extend",
+        "filter": {
+         
+        }
+    },
+    "auth": self.auth,
+    "id": self.times
+}
+		# res = requests.post(self.z_url, json=a_data)
+		res = self.commit(a_data)['result']
+		# print res.json()
+		for r in res:
+			print r['ip']
+
+def main():
+	Z = Zabbix()
+	Z.get_auth()
+	Z.get_hostip()
+
+
+if __name__ == '__main__':
+	main()
