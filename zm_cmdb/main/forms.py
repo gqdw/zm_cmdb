db22c53f3a573ac8a060347c285963a01e478bb8
gqdw <aca_jingru@qq.com>
2016-05-25 10:58:15 +0800

    change label name length

--- zm_cmdb/main/forms.py
+++ zm_cmdb/main/forms.py
@@ -6,6 +6,6 @@ class ApplyForm(forms.Form):
 	times = forms.IntegerField(label='台数', min_value=1, initial=1)
 	info = forms.CharField(label='备注', max_length=100)
 	sgroup = forms.CharField(label='安全组', initial='生产组', max_length=30)
-	allowdb = forms.CharField(label='需要连接的rds数据库', initial='xx.mysql.rds.aliyuncs.com', max_length=100)
+	allowdb = forms.CharField(label='需要连接的rds', initial='xx.mysql.rds.aliyuncs.com', max_length=100)
 	name = forms.CharField(label='申请人', max_length=30)
 	
