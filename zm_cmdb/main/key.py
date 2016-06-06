49ce718936d11066c7c34e56e8166dcd2090440b
gqdw <aca_jingru@qq.com>
2016-06-06 14:44:06 +0800

    use new key_filename

--- zm_cmdb/main/key.py
+++ zm_cmdb/main/key.py
@@ -7,7 +7,8 @@ def get_keys(hostname, username='root'):
 	client = paramiko.SSHClient()
 	client.load_system_host_keys()
 	client.set_missing_host_key_policy(paramiko.client.WarningPolicy())
-	client.connect(hostname, username=username)
+	keyfile = '/Users/gqdw/Downloads/pkey/id_rsa'
+	client.connect(hostname, username=username, key_filename=keyfile)
 	cmd1 = 'cat /root/.ssh/authorized_keys'
 	# cmd1 = 'cat %s' % os.path.expanduser('~/.ssh/authorized_keys')
 	print cmd1
@@ -17,6 +18,7 @@ def get_keys(hostname, username='root'):
 	# files = stdout.read().split()
 
 if __name__ == '__main__':
-	keys = get_keys('121.40.76.26')
+	keys = get_keys('114.55.72.105')
+	#keys = get_keys('121.40.76.26')
 	print keys
 
