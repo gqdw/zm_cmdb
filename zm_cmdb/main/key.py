cacca6e27d7a8639d8d830081260e7f019abbbdf
gqdw <aca_jingru@qq.com>
2016-06-06 14:26:16 +0800

    add key.py

---
+++ zm_cmdb/main/key.py
@@ -0,0 +1,22 @@
+import os
+import logging
+import paramiko
+#import socket
+
+def get_keys(hostname, username='root'):
+	client = paramiko.SSHClient()
+	client.load_system_host_keys()
+	client.set_missing_host_key_policy(paramiko.client.WarningPolicy())
+	client.connect(hostname, username=username)
+	cmd1 = 'cat /root/.ssh/authorized_keys'
+	# cmd1 = 'cat %s' % os.path.expanduser('~/.ssh/authorized_keys')
+	print cmd1
+	stdin, stdout, stderr = client.exec_command(cmd1)
+	keys = stdout.read().split('\n')
+	return keys
+	# files = stdout.read().split()
+
+if __name__ == '__main__':
+	keys = get_keys('121.40.76.26')
+	print keys
+
