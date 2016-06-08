cf69d4da982b2f1d0ec8f5050e536b3aa6b66ec9
gqdw <aca_jingru@qq.com>
2016-06-08 17:34:08 +0800

    use try except

--- zm_cmdb/main/key.py
+++ zm_cmdb/main/key.py
@@ -4,16 +4,22 @@ import paramiko
 #import socket
 
 def get_keys(hostname, username='root'):
-	client = paramiko.SSHClient()
-	client.load_system_host_keys()
-	client.set_missing_host_key_policy(paramiko.client.WarningPolicy())
-	keyfile = '/tmp/id_rsa'
-	client.connect(hostname, username=username, key_filename=keyfile)
-	cmd1 = 'cat /root/.ssh/authorized_keys'
-	# cmd1 = 'cat %s' % os.path.expanduser('~/.ssh/authorized_keys')
-	print cmd1
-	stdin, stdout, stderr = client.exec_command(cmd1)
-	keys = stdout.read().split('\n')
+	try:
+		client = paramiko.SSHClient()
+		client.load_system_host_keys()
+		client.set_missing_host_key_policy(paramiko.client.WarningPolicy())
+		keyfile = '/tmp/id_rsa'
+		client.connect(hostname, username=username, key_filename=keyfile, timeout=5)
+		cmd1 = 'cat /root/.ssh/authorized_keys'
+		# cmd1 = 'cat %s' % os.path.expanduser('~/.ssh/authorized_keys')
+		print cmd1
+		stdin, stdout, stderr = client.exec_command(cmd1)
+		keys = stdout.read().split('\n')
+	# except paramiko.ssh_exception.SSHException:
+	except Exception, e:
+		print e
+		keys = ''
+
 	return keys
 	# files = stdout.read().split()
 
