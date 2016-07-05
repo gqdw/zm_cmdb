0095105229c64e7b619cb34d91cd2acdea19a859
gqdw <aca_jingru@qq.com>
2016-07-05 18:48:32 +0800

    add function execmd

--- zm_cmdb/main/key.py
+++ zm_cmdb/main/key.py
@@ -3,28 +3,27 @@ import logging
 import paramiko
 # import socket
 
-
-def get_keys(hostname, username='root'):
+def execmd(hostname, username, cmd):
 	try:
 		client = paramiko.SSHClient()
 		client.load_system_host_keys()
-		client.set_missing_host_key_policy(paramiko.client.WarningPolicy())
+		client.set_missing_host_key_policy(paramiko.client.WarningPolicy()    )
 		keyfile = '/tmp/id_rsa'
-		client.connect(hostname, username=username, key_filename=keyfile, timeout=5)
-		cmd1 = 'cat /root/.ssh/authorized_keys'
-		# cmd1 = 'cat %s' % os.path.expanduser('~/.ssh/authorized_keys')
-		print cmd1
-		stdin, stdout, stderr = client.exec_command(cmd1)
-		keys = stdout.read().strip().split('\n')
-	# except paramiko.ssh_exception.SSHException:
+		client.connect(hostname, username=username, key_filename=keyfile,     timeout=5)
+		stdin, stdout, stderr = client.exec_command(cmd)
 	except Exception, e:
 		print e
-		keys = ''
+	return stdout.read()
 
-	return keys
-	# files = stdout.read().split()
+
+def get_keys(hostname, username='root'):
+	cmd = 'cat /root/.ssh/authorized_keys'
+	res = execmd('114.55.72.105', 'root', cmd)	
+	return res.strip().split('\n')
+
 
 if __name__ == '__main__':
 	keys = get_keys('114.55.72.105')
 # 	keys = get_keys('121.40.76.26')
+	print execmd('114.55.72.105', 'root', 'ls /tmp')
 	print keys
